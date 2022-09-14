5a96ccce024b4c7303262c5033d351767bde8bc5
Joao Paulo Silva Piauhy <joaopasip@gmail.com>
2022-09-14 16:02:35 -0300

    feat: :art: implements reqs. 1 até 3

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import 'express-async-errors';
+import globalError from './helpers/error/global.error';
 
 const app = express();
+app.use(globalError);
 
 export default app;
---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+import { vehicle } from './IVehicle';
+
+const car = vehicle.extend({
+  doorsQty: z.number().int().min(2).max(4),
+  seatsQty: z.number().int().min(2).max(7),
+});
+
+export type ICar = z.infer<typeof car>;
+export { car };
